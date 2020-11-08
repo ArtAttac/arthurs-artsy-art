b8e8d262991b4c5a3ef20a8ccca9ca45fce3cea2
Arthur Itzhaki <sovietbulldozer@gmail.com>
2020-11-08 04:13:17 -0500

    first commit

---
+++ src/styles.js
@@ -0,0 +1,16 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(() => ({
+  appBar: {
+    borderRadius: 15,
+    margin: '30px 0',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    marginLeft: '15px',
+    marginBottom: '10px'
+  },
+}));
